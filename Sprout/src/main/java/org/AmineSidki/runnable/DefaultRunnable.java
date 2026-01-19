7cbaa61fd3ae377ba7872423c87aa5bcee5ea9c9
AmineSidki <amineyoutubw@gmail.com>
2026-01-19 01:45:53 +0100

    FINALLY

--- Sprout/src/main/java/org/AmineSidki/runnable/DefaultRunnable.java
+++ Sprout/src/main/java/org/AmineSidki/runnable/DefaultRunnable.java
@@ -9,10 +9,12 @@ import org.AmineSidki.generator.MapperGenerator;
 import org.AmineSidki.generator.RepositoryGenerator;
 import org.AmineSidki.generator.ServiceGenerator;
 import org.AmineSidki.model.EntityMetadata;
+import org.AmineSidki.util.BannerPrinter;
 import org.AmineSidki.util.EntityParser;
 import picocli.CommandLine;
 
 import java.io.*;
+import java.time.LocalDateTime;
 
 @CommandLine.Command(name="default" , version = "0.1" , description = "Default Sprout workflow")
 public class DefaultRunnable implements Runnable{
@@ -22,6 +24,8 @@ public class DefaultRunnable implements Runnable{
 
     @Override
     public void run() {
+        BannerPrinter.print();
+
 
         File entityPackage = new File(defaultDir + "/entity");
         File[] files = entityPackage.listFiles();
@@ -46,7 +50,7 @@ public class DefaultRunnable implements Runnable{
         Mustache dtoMustache = mf.compile("templates/DtoTemplate.mustache");
         Mustache mapperMustache = mf.compile("templates/MapperTemplate.mustache");
 
-        System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold Generation service started : |@ \n"));
+        System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold Generating.. |@ \n"));
 
         for(File entity : files){
             if(!entity.isFile()){
@@ -59,15 +63,14 @@ public class DefaultRunnable implements Runnable{
 
                 //Generating
                 repoGen.generate(em , repoMustache , defaultDir);
-                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold,green -- Generated Repository for " + entity.getName() +" ! |@"));
+                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|faint " + LocalDateTime.now() + "|@ @|bold,blue  INFO|@ --- @|magenta [Sprout]|@ : Generating Repository for " + entity.getName()));
                 dtoGen.generate(em , dtoMustache , defaultDir);
-                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold,green -- Generated DTOs for " + entity.getName() +" ! |@"));
+                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|faint " + LocalDateTime.now() + "|@ @|bold,blue  INFO|@ --- @|magenta [Sprout]|@ : Generating DTOs for " + entity.getName()));
                 mapperGen.generate(em , mapperMustache , defaultDir);
-                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold,green -- Generated Mappers for " + entity.getName() +" ! |@"));
+                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|faint " + LocalDateTime.now() + "|@ @|bold,blue  INFO|@ --- @|magenta [Sprout]|@ : Generating Mapper for " + entity.getName()));
                 serviceGen.generate(em , serviceMustache , defaultDir);
-                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold,green -- Generated Service for " + entity.getName() +" ! |@"));
+                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|faint " + LocalDateTime.now() + "|@ @|bold,blue  INFO|@ --- @|magenta [Sprout]|@ : Generating Service for " + entity.getName()));
 
-                System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold,yellow --------------------------------------- |@"));
             } catch (FileNotFoundException e) {
                 throw new RuntimeException("An error occurred whilst reading files.");
             } catch (IOException e) {
@@ -75,6 +78,6 @@ public class DefaultRunnable implements Runnable{
             }
         }
 
-        System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold Generation service ended successfully ! |@ \n"));
+        System.out.println(CommandLine.Help.Ansi.AUTO.string("@|bold \nGeneration service ended successfully, shutting down ! |@ \n"));
     }
 }
---
+++ Sprout/src/main/java/org/AmineSidki/util/BannerPrinter.java
@@ -0,0 +1,24 @@
+package org.AmineSidki.util;
+
+import picocli.CommandLine.Help.Ansi;
+
+public class BannerPrinter {
+
+    private static final String SPROUT_BANNER =
+            "  ____                             _   \n" +
+                    " / ___|  _ __   _ __   ___   _   _ | |_ \n" +
+                    " \\___ \\ | '_ \\ | '__| / _ \\ | | | || __|\n" +
+                    "  ___) || |_) || |   | (_) || |_| || |_ \n" +
+                    " |____/ | .__/ |_|    \\___/  \\__,_| \\__|\n" +
+                    "        |_|                             ";
+
+    public static void print() {
+        // Use picocli's Ansi.AUTO to detect if colors should be used
+        String coloredBanner = Ansi.AUTO.string(
+                "@|bold,green " + SPROUT_BANNER + "|@\n" +
+                        "@|cyan  :: Sprout Generator :: |@ @|yellow (v1.0-MVP)|@\n"
+        );
+
+        System.out.println(coloredBanner);
+    }
+}
